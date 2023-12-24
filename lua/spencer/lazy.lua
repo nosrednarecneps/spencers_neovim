d16954640d6fae972253e2c126f522710614c140
nosrednarecneps <spencyboy2007@gmail.com>
2023-12-24 12:07:25 -0700

    configured mason

--- lua/spencer/lazy.lua
+++ lua/spencer/lazy.lua
@@ -11,5 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("spencer.plugins", {})
+require("lazy").setup({{import = "spencer.plugins"}, {import = "spencer.plugins.lsp"}})
 
